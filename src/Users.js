760445a5fc80fb57178beb50d8f450a3b6d42001
trinadhkalangi <trinadhkalangi@users.noreply.github.com>
2020-07-31 15:48:37 +0530

    css change

--- src/Users.js
+++ src/Users.js
@@ -12,7 +12,7 @@ function Users(props) {
     return (
         <div className="row m-5">
             {props.userData.map((user, index) => (
-                <div key={index} className="col-3 mb-2">
+                <div key={index} className="col-12 col-sm-3 mb-2">
 
                     <div className="card text-center" style={{ cursor: "Pointer" }} data-toggle="modal" data-target="#exampleModal" onClick={() => openModal(user.activity_periods)}>
                         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
